f8aae1fbb4a44703a758742d33613812bf51aa19
David Lee <ddlee.907@gmail.com>
2015-12-21 16:49:53 -0500

    m

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,25 +2,31 @@ class UsersController < ApplicationController
 
 
   def new
-    # authenticate!
+
     @user = User.new
   end
 
   def create
-    # authenticate!
+    
     user = User.create(user_params)
-    redirect_to profile_path
+    if user.save
+      session[:user_id] = user.id
+      redirect_to profile_path
+    else
+      flash[:error] = user.errors.full_messages
+      redirect_to new_user_path
+    end
   end
 
   def log_in
   end
 
   def admin
-    # authenticate!
+    authenticate!
   end
 
   def profile
-    # authenticate!
+    authenticate!
     if current_user.level == 0
       redirect_to admin_path
     end
