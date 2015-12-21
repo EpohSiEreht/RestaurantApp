9c204d2b56670649a3c3b01b877caa57c23357bb
David Lee <ddlee.907@gmail.com>
2015-12-21 16:35:21 -0500

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    authenticate!
+    # authenticate!
     user = User.create(user_params)
     if user.save
       session[:user_id] = user.id
@@ -22,11 +22,11 @@ class UsersController < ApplicationController
   end
 
   def admin
-    authenticate!
+    # authenticate!
   end
 
   def profile
-    authenticate!
+    # authenticate!
     if current_user.level == 0
       redirect_to admin_path
     end
