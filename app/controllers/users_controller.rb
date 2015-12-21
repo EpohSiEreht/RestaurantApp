15732111344d2c4e20d67ad2153495a5b387d9c5
David Lee <ddlee.907@gmail.com>
2015-12-21 16:36:57 -0500

    a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,13 @@ class UsersController < ApplicationController
   def create
     # authenticate!
     user = User.create(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to profile_path
-    else
-      flash[:error] = user.errors.full_messages
-      redirect_to new_user_path
-    end
+    # if user.save
+    #   session[:user_id] = user.id
+    #   redirect_to profile_path
+    # else
+    #   flash[:error] = user.errors.full_messages
+    #   redirect_to new_user_path
+    # end
   end
 
   def log_in
